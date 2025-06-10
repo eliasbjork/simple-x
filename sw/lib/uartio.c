18a15d7aad87000eb171fa485294dd96942493c2
eliasbjork <52861491+eliasbjork@users.noreply.github.com>
2025-06-10 14:29:11 +0200

    remove main and add header

--- sw/lib/uartio.c
+++ sw/lib/uartio.c
@@ -1,13 +1,15 @@
+#include "uartio.h"
+
 #define CLK_FREQ_HZ 0x8000103C
 #define UART_BASE   0x80002000
 
 #define BAUD_RATE   115200
 
-#define REG_BRDL    (4*0x00)   // Baud rate divisor (LSB)
-#define REG_IER     (4*0x01)   // Interrupt enable reg.
-#define REG_FCR     (4*0x02)   // FIFO control reg.
-#define REG_LCR     (4*0x03)   // Line control reg.
-#define REG_LSR     (4*0x05)   // Line status reg.
+#define REG_BRDL    (4*0x00)   // baud rate divisor (LSB)
+#define REG_IER     (4*0x01)   // interrupt enable reg
+#define REG_FCR     (4*0x02)   // FIFO control reg
+#define REG_LCR     (4*0x03)   // line control reg
+#define REG_LSR     (4*0x05)   // line status reg
 #define LCR_CS8     0x03       // 8 bits data size
 #define LCR_1_STB   0x00       // 1 stop bit
 #define LCR_PDIS    0x00       // parity disable
@@ -25,12 +27,11 @@ typedef unsigned int uint32_t;
 volatile uint8_t* uart_base = (volatile uint8_t*)UART_BASE;
 volatile uint32_t* clk_freq_hz = (volatile uint32_t*)CLK_FREQ_HZ;
 
-
 static inline void init_uart() {
-    // Set DLAB bit in LCR
+    // set DLAB bit in LCR
     *(uart_base + REG_LCR) = 0x80;
 
-    // Set divisor regs
+    // set divisor regs
     uint32_t baud_rate_divisor = *clk_freq_hz/BAUD_RATE >> 4;
     *(uart_base + REG_BRDL) = (uint8_t)baud_rate_divisor;
 
@@ -38,7 +39,7 @@ static inline void init_uart() {
     *(uart_base + REG_LCR) = LCR_CS8 | LCR_1_STB | LCR_PDIS;
     *(uart_base + REG_FCR) = FCR_FIFO | FCR_MODE0 | FCR_FIFO_8 | FCR_RCVRCLR | FCR_XMITCLR;
 
-    // disable interrupts 
+    // disable interrupts
     *(uart_base + REG_IER) = (uint8_t)0;
 }
 
@@ -50,16 +51,12 @@ void print(char* s) {
 
     uint8_t* c = (uint8_t*)s;
     while (*c) {
-        // Await space in UART FIFO
+        // await space in UART FIFO
         do {
             line_status = *(uart_base + REG_LSR);
         } while (!(line_status & LSR_THRE));
 
-        // Write byte to UART
+        // write byte to UART
         *uart_base = *c++;
     }
-}
-
-int main() {
-    print("Hello world!\n");
 }
---
+++ sw/lib/uartio.h
@@ -0,0 +1,6 @@
+#ifndef UARTIO_H
+#define UARTIO_H
+
+void print(char*);
+
+#endif // UARTIO_H
