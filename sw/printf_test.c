685b86dd6f08c8c5f9e3df48c0320235c59a2f9f
eliasbjork <52861491+eliasbjork@users.noreply.github.com>
2025-06-25 21:54:56 +0200

    enable printf (soft)float format

--- sw/printf_test.c
+++ sw/printf_test.c
@@ -1,9 +1,15 @@
 #include "ext/printf/printf.h"
 
 int main() {
-    int a = 10;
+    int i1 = 1;
+    int i2 = 2;
+    int i3 = i1+i2;
+    float f1 = 3.14;
+    float f2 = 2.71;
+    float f3 = f1+f2;
 
     for (;;) {
-        printf("a = %d\n", a);
+        printf("1 + 2 = %d\n", i3);
+        printf("3.14 + 2.71 = %f\n", f3);
     }
 }
