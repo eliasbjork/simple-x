e55f38fae8853be1ac837120e563c644633384be
eliasbjork <52861491+eliasbjork@users.noreply.github.com>
2025-10-14 19:49:13 +0200

    temporary solution to be able to build and run simplex in el2sim

---
+++ sw/simplex/el2sim_simplex.c
@@ -0,0 +1,355 @@
+#if defined(VEERWOLF)
+
+#include "../lib/fmath.h"
+#include "../lib/mem.h"
+#include "../lib/uartio.h"
+
+#elif defined(EL2SIM)
+
+#include "../lib/fmath.h"
+#include "../lib/mem.h"
+#include "../el2sim/hostio.h"
+
+#else
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
+
+#include "simplex.h"
+#include "util.h"
+
+
+static const float EPSILON = 1e-5;
+
+
+struct simplex_t {
+    int m;
+    int n;
+    int* var;    // n+m+1
+    float** a;   // m x n+1
+    float* b;    // m
+    float* x;    // n+m
+    float* c;    // n
+    float y;
+};
+
+
+float simplex(int m, int n, float** a, float* b, float* c, float* x, float y) {
+    return xsimplex(m, n, a, b, c, x, y, NULL, 0);
+}
+
+
+float xsimplex(int m, int n, float** a, float* b, float* c, float* x, float y, int* var, int h) {
+    simplex_t s;
+    int i, row, col;
+
+    if (!(initial(&s, m, n, a, b, c, x, y, var))) {
+        free(s.var);
+        return NAN;
+    }
+
+    while (col = select_nonbasic(s), col >= 0) {
+        row = -1;
+
+        // find which constraint is tightest for non-basic with index col
+        for (i = 0; i < m; i++) {
+            if (a[i][col] > EPSILON && (row < 0 || b[i]/a[i][col] < b[row]/a[row][col])) {
+                row = i;
+            }
+        }
+
+        // if non of the constraints limits non-basic with index col
+        if (row < 0) {
+            free(s.var);
+            return INFINITY;
+        }
+
+        pivot(&s, row, col);
+    }
+
+    if (h == 0) {
+        for (i = 0; i < n; i++) {
+            if (s.var[i] < n) {
+                x[s.var[i]] = 0;
+            }
+        }
+        for (i = 0; i < m; i++) {
+            if (s.var[n+i] < n) {
+                x[s.var[n+i]] = s.b[i];
+            }
+        }
+        free(s.var);
+    } else {
+        for (i = 0; i < n; i++)
+            x[i] = 0;
+        for (i = n; i < n+m; i++)
+            x[i] = s.b[i-n];
+    }
+    return s.y;
+}
+
+
+int initial(simplex_t* s, int m, int n, float** a, float* b, float* c, float* x, float y, int* var) {
+    int i,j,k;
+    float w;
+
+    k = init(s, m, n, a, b, c, x, y, var);
+
+    if (b[k] >= 0)
+        return 1; // feasible solution
+
+    prepare(s, k);
+    n = s->n;
+
+    s->y = xsimplex(m, n, s->a, s->b, s->c, s->x, 0, s->var, 1);
+
+    for (i = 0; i < m+n; i++)
+        if (s->var[i] == m+n-1) {
+            if (fabs(s->x[i]) > EPSILON) {
+                free(s->x);
+                free(s->c);
+                return 0; // infeasible
+            } else {
+                break;
+            }
+        }
+
+    if (i >= n) {
+        for (j = 0, k = 0; k < n; k++)
+            if (fabs(s->a[i-n][k]) > fabs(s->a[i-n][j]))
+                j = k;
+
+        pivot(s, i-n, j);
+        i = j;
+    }
+
+    if (i < n-1) {
+        k = s->var[i];
+        s->var[i] = s->var[n-1];
+        s->var[n-1] = k;
+
+        for (k = 0; k < m; k++) {
+            w = s->a[k][n-1];
+            s->a[k][n-1] = s->a[k][i];
+            s->a[k][i] = w;
+        }
+    }
+
+    free(s->c);
+    s->c = c;
+    s->y = y;
+
+    for (k = n-1; k < n+m-1; k++)
+        s->var[k] = s->var[k+1];
+
+    n = s->n-1;
+    s->n = s->n-1;
+
+    float* t = calloc(n, sizeof(float));
+
+    for (k = 0; k < n; k++) {
+        for (j = 0; j < n; j++)
+            if (k == s->var[j]) {
+                // x_k is nonbasic, add c_k
+                t[j] = t[j] + s->c[k];
+                goto next_k;
+            }
+
+        // x_k is basic
+        for (j = 0; j < m; j++)
+            if (s->var[n+j] == k)
+                // x_k is at row j
+                break;
+
+        s->y = s->y + s->c[k]*s->b[j];
+
+        for (i = 0; i < n; i++)
+            t[i] = t[i] - s->c[k]*s->a[j][i];
+
+        next_k:;
+    }
+
+    for (i = 0; i < n; i++)
+        s->c[i] = t[i];
+
+    free(t);
+    free(s->x);
+
+    return 1;
+}
+
+
+int init(simplex_t* s, int m, int n, float** a, float* b, float* c, float* x, float y, int* var) {
+    int i, k;
+
+    s->m = m;
+    s->n = n;
+    // s->var = var;
+    s->a = a;
+    s->b = b;
+    s->x = x;
+    s->c = c;
+    s->y = y;
+
+    if (var == NULL) {
+        var = calloc(m+n+1, sizeof(int)); // borde man checka så det inte blir NULL igen?
+
+        for (i = 0; i < m+n; i++)
+            var[i] = i;
+    }
+    s->var = var;
+
+    for (k = 0, i = 1; i < m; i++) {
+        if (b[i] < b[k])
+            k = i;
+    }
+
+    return k;
+}
+
+
+void prepare(simplex_t* s, int k) {
+    int m = s->m;
+    int n = s->n;
+    int i;
+
+    for (i = m+n; i > n; i--)
+        s->var[i] = s->var[i-1];
+
+    s->var[n] = m+n;
+
+    n = n+1;
+
+    for (i = 0; i < m; i++)
+        s->a[i][n-1] = -1;
+
+    s->x = calloc(m+n, sizeof(float));
+    s->c = calloc(n, sizeof(float));
+
+    s->c[n-1] = -1;
+    s->n = n;
+
+    pivot(s, k, n-1);
+}
+
+
+void pivot(simplex_t* s, int row, int col) {
+    float** a = s->a;
+    float* b = s->b;
+    float* c = s->c;
+    int m = s->m;
+    int n = s->n;
+    int i,j,t;
+
+    t = s->var[col];
+    s->var[col] = s->var[n+row];
+    s->var[n+row] = t;
+
+    s->y = s->y + c[col]*b[row]/a[row][col];
+
+    for (i = 0; i < n; i++)
+        if (i != col)
+            c[i] = c[i] - c[col]*a[row][i]/a[row][col];
+
+    c[col] = -c[col]/a[row][col];
+
+    for (i = 0; i < m; i++)
+        if (i != row)
+            b[i] = b[i] - a[i][col]*b[row]/a[row][col];
+
+    for (i = 0; i < m; i++)
+        if (i != row)
+            for (j = 0; j < n; j++)
+                if (j != col)
+                    a[i][j] = a[i][j] - a[i][col]*a[row][j]/a[row][col];
+
+    for ( i = 0; i < m; i++)
+        if (i != row)
+            a[i][col] = -a[i][col]/a[row][col];
+
+    for (i = 0; i < n; i++)
+        if (i != col)
+            a[row][i] = a[row][i]/a[row][col];
+
+    b[row] = b[row]/a[row][col];
+    a[row][col] = 1/a[row][col];
+}
+
+
+int select_nonbasic(simplex_t s) {
+    int i, max;
+
+    // Dantzig's rule i.e. always pick the steepest direction
+    // (i corresponding to largest c[i])
+
+    for (max = 0, i = 1; i < s.n; i++) {
+        if (s.c[max] < s.c[i]) {
+            max = i;
+        }
+    }
+
+    if (s.c[max] > EPSILON) {
+        return max;
+    }
+
+    return -1;
+}
+
+
+int main() {
+    int m;
+    int n;
+
+#ifdef VEERWOLF
+    for (;;) {
+#endif
+
+        printf("Starting Simplex\n");
+
+        // scanf("%d %d", &m, &n);
+        m = 4;
+        n = 4;
+
+        float** a = make_matrix(m, n+2);
+        // float b[m];
+        // float c[n];
+
+        float x[n+1];
+        float y = 0;
+
+        // scan_vec(c, n);
+        // scan_matrix(a, m, n);
+        // scan_vec(b, m);
+        // test/4/8/20008
+        float c[] = {0.0, 6.0, -8.0, 0.0};
+        a[0][0]=-3.0; a[0][1]= 5.0; a[0][2]=-7.0; a[0][3]= 5.0;
+        a[1][0]=-2.0; a[1][1]= 7.0; a[1][2]= 7.0; a[1][3]=-6.0;
+        a[2][0]=-6.0; a[2][1]= 8.0; a[2][2]=-1.0; a[2][3]= 5.0;
+        a[3][0]= 3.0; a[3][1]= 9.0; a[3][2]= 3.0; a[3][3]= 4.0;
+        float b[] = {0.0, 80.0, -30, 60};
+
+        float sol = simplex(m, n, a, b, c, x, y);
+
+
+        if (!isnan(sol) && !isinf(sol)) {
+            printf("z = %lf\n", sol);
+        } else if (isinf(sol)) {
+            // the problem is unbounded 
+            printf("z = inf\n");
+        } else {
+            // the problem is infeasible
+            printf("z = nan\n");
+        }
+
+        free_matrix(a, m);
+
+#ifdef VEERWOLF
+        _reset_heap();
+    }
+#endif
+
+    return 0;
+}
---
+++ sw/simplex/el2sim_util.c
@@ -0,0 +1,89 @@
+#if defined(VEERWOLF)
+
+#include "../lib/mem.h"
+#include "../lib/uartio.h"
+
+#elif defined(EL2SIM)
+
+#include "../lib/mem.h"
+#include "../el2sim/hostio.h"
+
+#else
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
+
+#include "util.h"
+
+
+float** make_matrix(int m, int n) {
+    float** a;
+    int i;
+
+    a = calloc(m, sizeof(float*));
+
+    for (i=0; i<m; i++) {
+        a[i] = calloc(n, sizeof(float));
+    }
+
+    return a;
+}
+
+
+void free_matrix(float** a, int nbr_rows) {
+    for (int i=0; i<nbr_rows; i++) {
+        free(a[i]);
+    }
+    free(a);
+}
+
+
+void print_vec(const float v[], int len) {
+    int i;
+    printf("\t");
+    for (i=0; i<len; i++) {
+        printf("%10.3f", v[i]);
+    }
+    printf("\n");
+}
+
+
+void print_matrix(float** const a, int m, int n) {
+    int i,j;
+    for (i=0; i<m; i++)
+    {
+        printf("\t");
+        for (j=0; j<n; j++) {
+            printf("%10.3f", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void print_obj_fun(const float c[], int len) {
+    int i;
+
+    printf("max z =\t");
+
+    for (i=0; i<len-1; i++) {
+        printf("%6.3f * x%d + ", c[i], i);
+    }
+
+    printf("%6.3f * x%d\n", c[i], i);
+}
+
+
+void print_constraints(float** const a, const float b[], int m, int n) {
+    int i,j;
+    for (i=0; i<m; i++)
+    {
+        printf("\t");
+        for (j=0; j<n-1; j++) {
+            printf("%6.3f * x%d + ", a[i][j], j);
+        }
+        printf("%6.3f * x%d\t\u2264 %6.3f \n", a[i][j], j, b[i]);
+    }
+}
