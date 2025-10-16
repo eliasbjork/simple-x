e8efd7d242136cb7503064e4bfacfaacbf15a9c2
eliasbjork <52861491+eliasbjork@users.noreply.github.com>
2025-10-16 22:51:09 +0200

    impl getch for el2sim

--- sw/el2sim/hostio.c
+++ sw/el2sim/hostio.c
@@ -1,7 +1,23 @@
 #include "hostio.h"
 
+#define GETCH_REQ_BYTE 0xf0
+
 extern volatile char tohost;
+
 
 void _putchar(char c) {
     tohost = c;
+}
+
+
+int getch() {
+    char read_val;
+
+    tohost = GETCH_REQ_BYTE; // request next input from el2sim tp_top
+
+    do {
+        read_val = tohost;
+    } while (read_val == GETCH_REQ_BYTE);
+
+    return (int)read_val;
 }
--- sw/el2sim/hostio.h
+++ sw/el2sim/hostio.h
@@ -2,7 +2,11 @@
 #define HOSTIO_H
 
 #include "../ext/printf/printf.h"
+#include "../ext/mini-scanf/c_scan.h"
+
+#define scanf c_scanf
 
 void _putchar(char);
+int getch();
 
 #endif // HOSTIO_H
